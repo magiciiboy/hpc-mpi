5929564dbb5b5c808c4f97d1518b6ab2bda234bb
Tung <tung.huynh@seedcom.vn>
2019-11-18 16:35:40 -0600

    Add assignment 1

---
+++ Codes/ping-pong/32/ping_pong_batch.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#SBATCH -N 32
+#SBATCH -n 32
+#SBATCH --ntasks-per-node 1
+#SBATCH -t 00:01:00
+#SBATCH -o output_32N_32n_1taskPerNode_time1Min_process_pinning_bindByCore_mapByNode
+
+echo "1B"
+echo "Blocking"
+mpirun -np 32 --bind-to core --map-by node ./blocking_SIZE1
+echo "NON Blocking"
+mpirun -np 32 --bind-to core --map-by node ./non_blocking_SIZE1
+
+echo "10B"
+echo "Blocking"
+mpirun -np 32 --bind-to core --map-by node ./blocking_SIZE10
+echo "NON Blocking"
+mpirun -np 32 --bind-to core --map-by node ./non_blocking_SIZE10
+
+echo "100B"
+echo "Blocking"
+mpirun -np 32 --bind-to core --map-by node ./blocking_SIZE100
+echo "NON Blocking"
+mpirun -np 32 --bind-to core --map-by node ./non_blocking_SIZE100
+
+echo "1KB"
+echo "Blocking"
+mpirun -np 32 --bind-to core --map-by node ./blocking_SIZE1000
+echo "NON Blocking"
+mpirun -np 32 --bind-to core --map-by node ./non_blocking_SIZE1000
+
+echo "10KB"
+echo "Blocking"
+mpirun -np 32 --bind-to core --map-by node ./blocking_SIZE10000
+echo "NON Blocking"
+mpirun -np 32 --bind-to core --map-by node ./non_blocking_SIZE10000
+
+echo "100KB"
+echo "Blocking"
+mpirun -np 32 --bind-to core --map-by node ./blocking_SIZE100000
+echo "NON Blocking"
+mpirun -np 32 --bind-to core --map-by node ./non_blocking_SIZE100000
+
+echo "1MB"
+echo "Blocking"
+mpirun -np 32 --bind-to core --map-by node ./blocking_SIZE1000000
+echo "NON Blocking"
+mpirun -np 32 --bind-to core --map-by node ./non_blocking_SIZE1000000
+
+echo "10MB"
+echo "Blocking"
+mpirun -np 32 --bind-to core --map-by node ./blocking_SIZE10000000
+echo "NON Blocking"
+mpirun -np 32 --bind-to core --map-by node ./non_blocking_SIZE10000000
+
+echo "100MB"
+echo "Blocking"
+mpirun -np 32 --bind-to core --map-by node ./blocking_SIZE100000000
+echo "NON Blocking"
+mpirun -np 32 --bind-to core --map-by node ./non_blocking_SIZE100000000
---
+++ Codes/ping-pong/56/submit.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sh compile.sh
+dos2unix ping_pong_batch.sh
+sbatch ping_pong_batch.sh
