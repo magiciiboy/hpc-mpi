90c07a397c541da5081e9ca50109f5bba7f9e1b5
Tung <tung.huynh@seedcom.vn>
2019-11-17 18:00:23 -0600

    Add run all script

---
+++ Codes/boradcast-reduce/run_all.sh
@@ -0,0 +1,13 @@
+SIZES=("-DONEMB" "-DTENMB" "-DHUNDREDMB")
+
+for (size in "${SIZES[@]}"); do
+    mpicc broadcast.c $size -o broad
+    mpicc reduce.c $size -o red
+
+    sh broad_red_2N_2n.sh
+    sh broad_red_4N_4n.sh
+    sh broad_red_8N_8n.sh
+    sh broad_red_16N_16n.sh
+    sh broad_red_28N_28n.sh
+    sh broad_red_56N_56n.sh
+done
