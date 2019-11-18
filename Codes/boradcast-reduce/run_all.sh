a6839d80133463eda738d53e5caf75af2546243e
Tung <tung.huynh@seedcom.vn>
2019-11-17 18:15:53 -0600

    Add run all script

--- Codes/boradcast-reduce/run_all.sh
+++ Codes/boradcast-reduce/run_all.sh
@@ -1,13 +1,33 @@
-SIZES=("-DONEMB" "-DTENMB" "-DHUNDREDMB")
+echo "1 MB"
+mpicc broadcast.c -DONEMB -o broad
+mpicc reduce.c -DONEMB -o red
 
-for size in "${SIZES[@]}"; do
-    mpicc broadcast.c $size -o broad
-    mpicc reduce.c $size -o red
+sh broad_red_2N_2n.sh
+sh broad_red_4N_4n.sh
+sh broad_red_8N_8n.sh
+sh broad_red_16N_16n.sh
+sh broad_red_28N_28n.sh
+sh broad_red_56N_56n.sh
 
-    sh broad_red_2N_2n.sh
-    sh broad_red_4N_4n.sh
-    sh broad_red_8N_8n.sh
-    sh broad_red_16N_16n.sh
-    sh broad_red_28N_28n.sh
-    sh broad_red_56N_56n.sh
-done
+echo "10 MB"
+mpicc broadcast.c -DTENMB -o broad
+mpicc reduce.c -DTENMB -o red
+
+sh broad_red_2N_2n.sh
+sh broad_red_4N_4n.sh
+sh broad_red_8N_8n.sh
+sh broad_red_16N_16n.sh
+sh broad_red_28N_28n.sh
+sh broad_red_56N_56n.sh
+
+echo "100 MB"
+mpicc broadcast.c -DHUNDREDMB -o broad
+mpicc reduce.c -DHUNDREDMB -o red
+
+sh broad_red_2N_2n.sh
+sh broad_red_4N_4n.sh
+sh broad_red_8N_8n.sh
+sh broad_red_16N_16n.sh
+sh broad_red_28N_28n.sh
+sh broad_red_56N_56n.sh
+
