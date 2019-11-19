08afaa1fe9655bf499637f806d73e650d5d50e20
Tung <tung.huynh@seedcom.vn>
2019-11-19 02:38:35 -0600

    Minor update

--- Codes/quicksort/submit.sh
+++ Codes/quicksort/submit.sh
@@ -5,6 +5,9 @@
 #SBATCH -t 00:02:00
 #SBATCH -o output_4N_4n_1taskPerNode_time1Min_process_pinning_bindByCore_mapByNode
 
+echo "Sort SMALL array"
 mpirun -np 4 ./quicksort_parallel_SMALL
+echo "Sort MEDIUM array"
 mpirun -np 4 ./quicksort_parallel_MEDIUM
+echo "Sort LARGE array"
 mpirun -np 4 ./quicksort_parallel_LARGE
